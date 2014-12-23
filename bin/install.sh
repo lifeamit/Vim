eaed9832a44e0531924c7f980d9b765821d8010e
Amit Agarwal <amitagwl@yahoo-inc.com>
2014-12-23 13:08:46 +0530

    mv to rm

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,7 @@
-mv ~/.vim ~/.vim_bkuptoday
-mv ~/.vimrc ~/.vimrc_bkuptoday
+rm -rf ~/.vim
+rm -rf ~/.vimrc
 mkdir -p ~/.vim
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~
 vim +PluginInstall +qall
-
-
